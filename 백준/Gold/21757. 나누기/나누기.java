d06e4d59d8d790b7bed4987823170826f865913e
taejeongH <taejeongh98@gmail.com>
2026-03-11 10:14:56 +0900

    [Gold II] Title: 나누기, Time: 292 ms, Memory: 27444 KB, Score: 60 point -BaekjoonHub

--- 백준/Gold/21757. 나누기/나누기.java
+++ 백준/Gold/21757. 나누기/나누기.java
@@ -1,11 +1,12 @@
-//package BOJ.나누기;
+// BOJ.나누기;
 
 import java.io.*;
 import java.util.*;
 
 public class Main{
-	static int N;
+	static int N, sum;
 	static int[] sumArr;
+	static long[][] dp;
 
 	public static void main(String[] args) throws IOException{
 		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
@@ -15,28 +16,31 @@ public class Main{
 		sumArr = new int[N+1];
 		st = new StringTokenizer(br.readLine());
 		for (int i=1; i<=N; i++) {
-			sumArr[i] = sumArr[i-1] + Integer.parseInt(st.nextToken());
+			sumArr[i] = sumArr[i-1] + (Integer.parseInt(st.nextToken()));
 		}
+		sum = sumArr[N]/4;
+		dp = new long[4][N+1];
+		for (int i=0; i<4; i++) Arrays.fill(dp[i], -1);
+		System.out.println(dfs(0, 0));
+//		for (int i=0; i<4; i++) System.out.println(Arrays.toString(dp[i]));
 		
-		int res = 0;
-		for (int i=1; i<=N; i++) {
-			if(sumArr[i] - sumArr[0] == sumArr[N] - sumArr[i]) {
-				res += dfs(1, i) * dfs(i+1, N);
-			}
-		}
-		System.out.println(res);
 	}
 
-	static int dfs(int l, int r) {
-		int res = 0;
+	static long dfs(int depth, int start) {
+		if (dp[depth][start]!=-1) return dp[depth][start];
+		if (depth == 3) {
+			if (sumArr[N] - sumArr[start] == sum) return dp[depth][start]=1;
+			else return dp[depth][start]=0;
+		}
 		
-		for (int i=l; i<=r; i++) {
-			if(sumArr[i] - sumArr[l-1] == sumArr[r] - sumArr[i]) {
-				res++;
+		long res = 0;
+		for (int i=start+1; i<N; i++) {
+			if (sumArr[i] - sumArr[start] == sum) {
+				res += dfs(depth+1, i);
 			}
 		}
-		return res;
 		
+		return dp[depth][start]=res;
 	}
 	
 }
