626281bad29caf758ee9b9cdeab90deb761fe653
taejeongH <taejeongh98@gmail.com>
2026-05-12 10:19:41 +0900

    [level 2] Title: 타겟 넘버, Time: 4.48 ms, Memory: 3.82 MB -BaekjoonHub

--- 프로그래머스/2/43165. 타겟 넘버/README.md
+++ 프로그래머스/2/43165. 타겟 넘버/README.md
@@ -1,6 +1,6 @@
 # [level 2] 타겟 넘버 - 43165 
 
-[문제 링크](https://school.programmers.co.kr/learn/courses/30/lessons/43165?language=cpp) 
+[문제 링크](https://school.programmers.co.kr/learn/courses/30/lessons/43165) 
 
 ### 성능 요약
 
@@ -16,7 +16,7 @@
 
 ### 제출 일자
 
-2026년 05월 12일 10:19:18
+2026년 05월 12일 10:19:40
 
 ### 문제 설명
 
--- 프로그래머스/2/43165. 타겟 넘버/타겟 넘버.cpp
+++ 프로그래머스/2/43165. 타겟 넘버/타겟 넘버.cpp
@@ -3,26 +3,7 @@
 
 using namespace std;
 
-static int N, t;
-static int arr[20];
-
-int dfs(int idx, int sum) {
-    if (idx == N) {
-        if (sum == t) return 1;
-        return 0;
-    }
-    
-    return dfs(idx+1, sum+arr[idx]) + dfs(idx+1, sum-arr[idx]);
-}
-
 int solution(vector<int> numbers, int target) {
     int answer = 0;
-    N = numbers.size();
-    
-    for (int i=0; i<N; i++) {
-        arr[i] = numbers[i];
-    }
-    t = target;
-    return dfs(0, 0);
+    return answer;
 }
-
