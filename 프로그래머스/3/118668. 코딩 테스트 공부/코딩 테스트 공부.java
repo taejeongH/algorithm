643733241a370069336e1b91b588b2744e41630e
taejeongH <taejeongh98@gmail.com>
2026-06-18 08:38:53 +0900

    [level 3] Title: 코딩 테스트 공부, Time: 9.42 ms, Memory: 57.4 MB -BaekjoonHub

--- 프로그래머스/3/118668. 코딩 테스트 공부/코딩 테스트 공부.java
+++ 프로그래머스/3/118668. 코딩 테스트 공부/코딩 테스트 공부.java
@@ -1,44 +1,68 @@
 import java.util.*;
+
 class Solution {
     int aMax, cMax, N;
     int[][] problems;
     final int INF = 1_000_000_000;
     int[][] dp;
+
     public int solution(int alp, int cop, int[][] problems) {
         this.problems = problems;
         N = problems.length;
-        
-        for (int i=0; i<N; i++) {
-            aMax = Math.max(problems[i][0], aMax);
-            cMax = Math.max(problems[i][1], cMax);
+
+        for (int[] p : problems) {
+            aMax = Math.max(aMax, p[0]);
+            cMax = Math.max(cMax, p[1]);
         }
-        dp = new int[aMax+500][cMax+500];
-        for (int i=0; i<aMax+500; i++) Arrays.fill(dp[i], -1);
-        
+
+        alp = Math.min(alp, aMax);
+        cop = Math.min(cop, cMax);
+
+        dp = new int[aMax + 1][cMax + 1];
+
+        for (int i = 0; i <= aMax; i++) {
+            Arrays.fill(dp[i], -1);
+        }
+
         return dfs(alp, cop);
     }
-    
+
     int dfs(int algo, int code) {
-        if (algo >= aMax && code >= cMax) return 0;
-        if(dp[algo][code] != -1) return dp[algo][code];
-        
+        if (algo == aMax && code == cMax) return 0;
+
+        if (dp[algo][code] != -1) return dp[algo][code];
+
         int res = INF;
-        
-        if (algo + 1 <= aMax) res = Math.min(dfs(algo+1, code)+1, res);
-        if (code + 1 <= cMax) res = Math.min(dfs(algo, code+1)+1, res);
-        
-        for (int i=0; i<N; i++) {
-            int algoReq = problems[i][0];
-            int codeReq = problems[i][1];
-            int algoInc = problems[i][2];
-            int codeInc = problems[i][3];
-            int cost = problems[i][4];
-            
-            if (algoReq <= algo && codeReq <= code && algo+algoInc<aMax+500 && code+codeInc<cMax+500) 
-                res = Math.min(dfs(algo+algoInc, code+codeInc)+cost, res);
+
+        // 알고력 1 올리기
+        if (algo < aMax) {
+            res = Math.min(res, dfs(algo + 1, code) + 1);
         }
-        
-        return dp[algo][code]=res;
+
+        // 코딩력 1 올리기
+        if (code < cMax) {
+            res = Math.min(res, dfs(algo, code + 1) + 1);
+        }
+
+        // 문제 풀기
+        for (int[] p : problems) {
+            int algoReq = p[0];
+            int codeReq = p[1];
+            int algoInc = p[2];
+            int codeInc = p[3];
+            int cost = p[4];
+
+            if (algo >= algoReq && code >= codeReq) {
+                int nextAlgo = Math.min(aMax, algo + algoInc);
+                int nextCode = Math.min(cMax, code + codeInc);
+
+                // 보상이 0,0인 이상한 문제 방어
+                if (nextAlgo == algo && nextCode == code) continue;
+
+                res = Math.min(res, dfs(nextAlgo, nextCode) + cost);
+            }
+        }
+
+        return dp[algo][code] = res;
     }
-    
 }
